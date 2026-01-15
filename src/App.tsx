4fb80364e4e807a05e7b4feb59eba421091407ee
yashchauhan2102 <yashchauhan2102@gmail.com>
2026-01-15 19:56:17 +0530

    feat(ui): fetch movies from api with loading and error states

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,61 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { MovieList } from "./components/MovieList";
 import { FilterBar } from "./components/FilterBar";
 import { movieList } from "./data/movies";
 import { filterMovies } from "./utils/filterMovies";
+import type { IMovie } from "./types/movie";
+import { MOVIE_URL } from "./constants/urls";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [selectedGenre, setSelectedGenre] = useState<string>("All");
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
   const filteredMovies = useMemo(
     () => filterMovies(movieList, searchTerm, selectedGenre),
     [movieList, searchTerm, selectedGenre]
   );
 
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch(MOVIE_URL);
+
+        const data = await response.json();
+
+        if (data.Response === "False") {
+          throw new Error(data.Error);
+        }
+
+        setMovies(data.Search);
+      } catch (err) {
+        setError((err as Error).message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading Movies...</p>;
+  }
+
+  if (error) {
+    return (
+      <>
+        <p>Error while fetching movies !! Please try again Later</p>
+        <p>Error: {error}</p>
+      </>
+    );
+  }
   return (
     <div className="app_container">
       <FilterBar
---
+++ src/constants/urls.ts
@@ -0,0 +1,2 @@
+export const MOVIE_URL: string =
+  "http://www.omdbapi.com/?apikey=4dfa27a0&s=bat";
